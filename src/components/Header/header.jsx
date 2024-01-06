c85626fce40cd60c09e499e62d5e0cd38bd74a3b
FATEMEH <imfa.cod@gmail.com>
2024-01-06 23:18:19 +0330

    Completion the content box of header

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -5,6 +5,14 @@ export default function Header() {
   return (
     <header className='header'>
       <NavBar></NavBar>
+
+      <div className='content_box'>
+        <div>
+          <h1 className='content_box_title'>PERSONAL APPROACH</h1>
+          <p className='content_box_text'>Lorem ipsum dolor amet consectetur adipiscing dolore magna aliqua enim minim estudiat veniam siad venumus dolore</p>
+        </div>
+        <a className='content_box_btn' href="">Take a Tour</a>
+      </div>
     </header>
   )
 }
