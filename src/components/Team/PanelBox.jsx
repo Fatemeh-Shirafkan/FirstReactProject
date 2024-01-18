eefea702a029c5bfbadfce8cd3e12dc14f2f0fc4
FATEMEH <imfa.cod@gmail.com>
2024-01-18 22:21:07 +0330

    Dynamic content of team panel boxes

--- src/components/Team/PanelBox.jsx
+++ src/components/Team/PanelBox.jsx
@@ -1,19 +1,17 @@
 import React from 'react'
 import './Team.css'
 
-export default function PanelBox() {
+export default function PanelBox({title, description}) {
   return (
 
     <div className='panel'>
         <div className='panel_header'>
             <h4>
-                <a href="#">Expert Research</a>
+                <a href="#">{title}</a>
             </h4>
         </div>
         <div className='panel_body'>
-            <div>
-            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch.
-            </div>
+            <div>{description}</div>
         </div>
     </div>
 
--- src/components/Team/Team.jsx
+++ src/components/Team/Team.jsx
@@ -4,6 +4,13 @@ import LeftIndicator from '../../../public/Icons/LeftIndicator'
 import PanelBox from './PanelBox.jsx'
 
 export default function Team() {
+
+    let PanelBoxInfo = [
+        {title:'Expert Research' ,description: 'Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch'},
+        {title:'Hi-Tech Engineering' ,description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'},
+        {title:'Outstanding Results' ,description: ' Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'},
+    ]
+
   return (
     <section className='light_bg'>
         <div className='container team_box'>
@@ -18,9 +25,9 @@ export default function Team() {
                 <div className='col-md-5 col-sm-7 ml-2 panel_col'>
                     <div>
                         <div>
-                            <PanelBox/>
-                            <PanelBox/>
-                            <PanelBox/>
+                            <PanelBox {...PanelBoxInfo[0]}/>
+                            <PanelBox {...PanelBoxInfo[1]}/>
+                            <PanelBox {...PanelBoxInfo[2]}/>
                         </div>
                     </div>
                 </div>
