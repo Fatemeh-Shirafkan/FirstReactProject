b02f38f837c98c0af28f93f1cacd79fa67ec554a
FATEMEH <imfa.cod@gmail.com>
2024-01-21 15:30:56 +0330

    Completing the basic structure of box services and creating an array of dynamization of boxes

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -2,11 +2,29 @@ import React from 'react'
 import './Services.css'
 import ServicesBox from './ServicesBox.jsx'
 
+
+let ServicesBoxInfo = [
+    {title:'Company Formation', description:'Lorem ipsum dolor amet consectetur ut consequat siad esqudiat dolor',},
+    {title:'Stuff Management', description:'Lorem ipsum dolor amet consectetur ut consequat siad esqudiat dolor',},
+    {title:'KPI Optimization', description:'Lorem ipsum dolor amet consectetur ut consequat siad esqudiat dolor',},
+    {title:'Information Security', description:'Lorem ipsum dolor amet consectetur ut consequat siad esqudiat dolor',},
+    {title:'Recrutinment', description:'Lorem ipsum dolor amet consectetur ut consequat siad esqudiat dolor',},
+    {title:'Materials Supply', description:'Lorem ipsum dolor amet consectetur ut consequat siad esqudiat dolor',},
+]
+
+
 export default function Services() {
   return (
-    <section>
-        <div>
-
+    <section className='Services_sct'>
+        <div className='container'>
+            <div className='row'>
+                <ServicesBox/>
+                <ServicesBox/>
+                <ServicesBox/>
+                <ServicesBox/>
+                <ServicesBox/>
+                <ServicesBox/>
+            </div>
         </div>
     </section>
   )
--- src/components/Services/ServicesBox.jsx
+++ src/components/Services/ServicesBox.jsx
@@ -4,6 +4,17 @@ import './Services.css'
 
 export default function ServicesBox() {
   return (
-    <div>ServicesBox</div>
+    <div className='col-sm-4 mb-sm-2'>
+        <div class="" >
+            <div class="service-element">
+                
+            </div>
+            <div class="service-info">
+                <h3 class="">Stuff Management</h3>
+                <p class="">Lorem ipsum dolor amet consectetur ut consequat siad esqudiat dolor</p>
+            </div>
+            <a href="#" class=""></a>    
+        </div>
+    </div>
   )
 }
