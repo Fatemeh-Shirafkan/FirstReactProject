e87e609f227715e5a03ac21f78ca98d70f4ac2b5
FATEMEH <imfa.cod@gmail.com>
2024-01-24 23:14:51 +0330

    Creating the initial structure of the Footer section

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -2,28 +2,37 @@ import React from 'react'
 import './Services.css'
 import ServicesBox from './ServicesBox.jsx'
 
+// import laboratory from '../../../public/Icons/ServicesBox/laboratory.jsx'
+// import noteBook from '../../../public/Icons/ServicesBox/noteBook.jsx'
+// import medal from '../../../public/Icons/ServicesBox/medal.jsx'
+// import clock from '../../../public/Icons/ServicesBox/clock.jsx'
+// import phone from '../../../public/Icons/ServicesBox/phone.jsx'
 
-let ServicesBoxInfo = [
-    {title:'Company Formation', description:'Lorem ipsum dolor amet consectetur ut consequat siad esqudiat dolor',},
-    {title:'Stuff Management', description:'Lorem ipsum dolor amet consectetur ut consequat siad esqudiat dolor',},
-    {title:'KPI Optimization', description:'Lorem ipsum dolor amet consectetur ut consequat siad esqudiat dolor',},
-    {title:'Information Security', description:'Lorem ipsum dolor amet consectetur ut consequat siad esqudiat dolor',},
-    {title:'Recrutinment', description:'Lorem ipsum dolor amet consectetur ut consequat siad esqudiat dolor',},
-    {title:'Materials Supply', description:'Lorem ipsum dolor amet consectetur ut consequat siad esqudiat dolor',},
-]
+
+
 
 
 export default function Services() {
+
+    // let ServicesBoxInfo = [
+    //     {title:'Company Formation', description:'Lorem ipsum dolor amet consectetur ut consequat siad esqudiat dolor', icon: ,},
+    //     {title:'Stuff Management', description:'Lorem ipsum dolor amet consectetur ut consequat siad esqudiat dolor', icon: ,},
+    //     {title:'KPI Optimization', description:'Lorem ipsum dolor amet consectetur ut consequat siad esqudiat dolor', icon:,},
+    //     {title:'Information Security', description:'Lorem ipsum dolor amet consectetur ut consequat siad esqudiat dolor', icon: ,},
+    //     {title:'Recrutinment', description:'Lorem ipsum dolor amet consectetur ut consequat siad esqudiat dolor', icon: ,},
+    //     {title:'Materials Supply', description:'Lorem ipsum dolor amet consectetur ut consequat siad esqudiat dolor', icon: ,},
+    // ]
+    
   return (
     <section className='Services_sct'>
         <div className='container'>
             <div className='row'>
-                <ServicesBox/>
-                <ServicesBox/>
-                <ServicesBox/>
-                <ServicesBox/>
-                <ServicesBox/>
-                <ServicesBox/>
+                <ServicesBox {...ServicesBoxInfo[0]}/>
+                <ServicesBox {...ServicesBoxInfo[1]}/>
+                <ServicesBox {...ServicesBoxInfo[2]}/>
+                <ServicesBox {...ServicesBoxInfo[3]}/>
+                <ServicesBox {...ServicesBoxInfo[4]}/>
+                <ServicesBox {...ServicesBoxInfo[5]}/>
             </div>
         </div>
     </section>
--- src/components/Services/ServicesBox.jsx
+++ src/components/Services/ServicesBox.jsx
@@ -1,17 +1,23 @@
 import React from 'react'
 import './Services.css'
 
+// import laboratory from '../../../public/Icons/ServicesBox/laboratory.jsx'
+// import noteBook from '../../../public/Icons/ServicesBox/noteBook.jsx'
+// import medal from '../../../public/Icons/ServicesBox/medal.jsx'
+// import clock from '../../../public/Icons/ServicesBox/clock.jsx'
+// import phone from '../../../public/Icons/ServicesBox/phone.jsx'
 
-export default function ServicesBox() {
+
+export default function ServicesBox({title, description, icon}) {
   return (
     <div className='col-sm-4 mb-sm-2'>
         <div class="" >
             <div class="service-element">
-                
+                {icon}
             </div>
             <div class="service-info">
-                <h3 class="">Stuff Management</h3>
-                <p class="">Lorem ipsum dolor amet consectetur ut consequat siad esqudiat dolor</p>
+                <h3 class="">{title}</h3>
+                <p class="">{description}</p>
             </div>
             <a href="#" class=""></a>    
         </div>
